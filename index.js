2491ab3ced1d11a780d5c69ccad90b7bd482de90
Mahdi <mahlledz.gmail.com>
2022-04-08 15:08:46 +0100

    1.0

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/licence', licenceRoutes);
 
-const DB_CONNECTION_URL = 'mongodb://0.0.0.0:27018/licence-db';
+const DB_CONNECTION_URL = 'mongodb://127.0.0.1:27017/licence';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(DB_CONNECTION_URL, {
